722fbc629143b0e6fb4908aec137aa84590cabdd
Kwok Yee Chu <k.y.chu96@gmail.com>
2018-11-11 15:30:36 +0000

    added comments, changed some values regarding the probabilities

--- MarkovChain.h
+++ MarkovChain.h
@@ -4,8 +4,12 @@
 
 extern "C"
 {
+	// calculate the weather state
 	DllExport int calculateWeatherState();
+	// return the probability
 	DllExport double getProbability();
+	// return a random number 
 	DllExport double getRandom(double min, double max);
+	// calculate the transition
 	DllExport int calculateTransition(int index);
 }
